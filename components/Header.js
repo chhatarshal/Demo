d269bd908508c80a9dd0ab65ea07cd31eb6a0ce2
chhatarshal <chauhan.natwal@hotmail.com>
2021-12-27 20:34:50 +0530

    removed image logo
    removed image logo

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,3 @@
- import Image from 'next/Image';
 import Router from 'next/router';
 
  const Header = () => {
@@ -6,9 +5,7 @@ import Router from 'next/router';
     return (
         <div className="flex justify-between py-4 px-2  lg:w-full h-40">
             <div onClick={() => {Router.push('/')}}>
-                <Image src="/quarklogo.png"
-                width={50} height={50} 
-                alt='logo' />
+               LOGO
             </div>
             <div>User</div>
         </div>
